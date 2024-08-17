906c6c75b2e7152ebda5ae2aca6425f6181349bf
Sarfaraz sk <sarfarazsk991@gmail.com>
2024-08-17 13:24:00 +0530

    ....

--- routes/login.js
+++ routes/login.js
@@ -11,4 +11,23 @@ router.get("/",(req,res)=>{
     }
 });
 
+// Save user email and password
+router.post("/",async(req,res)=>{
+    const{email,password} = req.body;
+    try{
+        const newUser = new user({
+            email,
+            password
+        })
+
+        await newUser.save();
+        console.log("Email and passwor is saved");
+        res.status(200).redirect("/form");
+        
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: "Internal server error"});
+    }
+})
+
 module.exports = router;
